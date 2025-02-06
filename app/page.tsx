bfe199c37b2a2fcce8f543b1b48caa64377b4ed2
Ahnaaf Rafee <ahnaafarafee@gmail.com>
2025-02-06 13:49:21 +0600

    intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/hero";
+import Intro from "@/components/intro";
 import Nav from "@/components/nav";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Nav />
       <Hero />
+      <Intro />
     </div>
   );
 }
---
+++ components/intro.tsx
@@ -0,0 +1,20 @@
+export default function Intro() {
+  return (
+    <div className="max-w-3xl mx-auto px-4 py-8 text-center">
+      <h1 className="text-4xl font-bold mb-4 capitalize">
+        Welcome to the website of Bionic-7
+      </h1>
+      <h6 className="text-lg text-gray-600 mb-4">
+        Biomedical Engineering Batch 7, Islamic University, Bangladesh
+      </h6>
+      <p className="text-base text-gray-800 leading-relaxed">
+        This website serves as the official academic portal for Batch 7 of the
+        Biomedical Engineering Department at Islamic University, Bangladesh.
+        Here, you will find a comprehensive repository of educational materials,
+        including lecture slides, textbooks, and additional academic resources.
+        Moreover, the site provides timely notifications, announcements, and
+        information on events that are pertinent to our batch.
+      </p>
+    </div>
+  );
+}
